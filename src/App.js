a1e182cfe9257317b7693dee4639b1e9026a7e7d
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-01 12:07:02 +0300

    Fix conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+// eslint-disable-next-line
 import { Component } from 'react';
 // eslint-disable-next-line
 import DisplayTodo from './components/DisplayTodo';
 // eslint-disable-next-line
 import AddToDO from './components/AddToDo';
+// eslint-disable-next-line
+import Header from './components/Header';
+// eslint-disable-next-line
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { todo: [] };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify(this.state.todo));
+    } else {
+      this.setState({
+        todo: JSON.parse(localStorage.getItem('todos')),
+      });
+    }
   }
 
   onHandleAddToDO = (content) => {
@@ -25,13 +40,46 @@ export default class App extends Component {
     localStorage.setItem('todos', JSON.stringify(todo));
   }
 
+  onHandleCheckBoxChange = (id) => {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    todos.forEach((todo) => {
+      if (todo.id === id) {
+        todo.complete = !todo.complete;
+      }
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  onDeleteHandler = (id) => {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    let selectedTodo = null;
+    todos.forEach((item) => {
+      if (item.id === id) {
+        selectedTodo = item;
+      }
+    });
+    if (selectedTodo.complete) {
+      const filteredToDos = todos.filter((item) => item.id !== id);
+      localStorage.setItem('todos', JSON.stringify(filteredToDos));
+      this.setState({
+        todos: JSON.parse(localStorage.getItem('todos')),
+      });
+    }
+  }
+
   render() {
     return (
+
       <div className="App" >
-        <AddToDO onHandleAddToDO={this.onHandleAddToDO} />
-        <DisplayTodo />
-        <button>Clear All Selected</button>
-      </div>
+        <NavBar />
+        <Header />
+        <div className='app-container'>
+          <AddToDO onHandleAddToDO={this.onHandleAddToDO} />
+          <DisplayTodo todos={this.state.todo}
+          onDeleteHandler={this.onDeleteHandler}
+          onHandleCheckBoxChange={this.onHandleCheckBoxChange} />
+        </div>
+        </div>
     );
   }
 }
--- src/components/DisplayTodo.js
+++ src/components/DisplayTodo.js
@@ -1,16 +1,30 @@
 // eslint-disable-next-line
 import React from 'react';
+// eslint-disable-next-line
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// eslint-disable-next-line
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
 
-const DisplayTodo = () => {
+const DisplayTodo = (props) => {
+  const { onHandleCheckBoxChange, onDeleteHandler } = props;
   const todos = JSON.parse(localStorage.getItem('todos'));
 
-  const todoList = todos.map((todo) => (
-        <div className='content-container' key={todo.id}>
-            <input type='checkbox' />
-            <p className='content-paragraph'>{todo.content}</p>
+  const onhandleDelete = (id) => {
+    onDeleteHandler(id);
+  };
 
+  let todoList = null;
+
+  if (todos !== null) {
+    todoList = todos.map((todo) => (
+        <div className='content-container' key={todo.id}>
+          <input id={todo.id} onChange={() => { onHandleCheckBoxChange(todo.id); }} type='checkbox' />
+          <p className='content-paragraph'>{todo.content}</p>
+          <FontAwesomeIcon onClick={() => { onhandleDelete(todo.id); }} className='trash-icons' icon={icon({ name: 'trash', family: 'sharp', style: 'solid' })} />
         </div>
-  ));
+    ));
+  }
+
   return (
     <div className="todo-container">
         {todoList}
